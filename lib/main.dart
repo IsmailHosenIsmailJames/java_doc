92c237befc52ca0c1efb102e02e4ae5f987d9c8f
MD Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-02-08 00:58:38 +0600

    logo and splash is done

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+import 'rust_doc.dart';
+
+final localhostServer =
+    InAppLocalhostServer(documentRoot: 'assets', port: 1502);
+
+Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
+
+  if (!kIsWeb) {
+    await localhostServer.start();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: const InAppWebViewExampleScreen(),
+    );
+  }
+}
